d9cc139328d0be380a407159c866b7dad8f5f736
Ragini K <karanderagini@gmail.com>
2022-05-12 10:39:26 +0530

    structure of android application in first demo

---
+++ app/src/main/java/com/example/firstdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.firstdemo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+}
